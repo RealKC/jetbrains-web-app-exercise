81b82ea42a62a9e1880e55929aa3ae41125348b2
Mițca Dumitru <dumitru0mitca@gmail.com>
2024-10-20 21:14:59 +0300

    Initialize a "Hello world" axum project

---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use axum::Router;
+use axum::routing::get;
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+#[tokio::main]
+async fn main() {
+    tracing_subscriber::registry()
+        .with(
+            tracing_subscriber::EnvFilter::try_from_default_env()
+                .unwrap_or_else(|_| format!("{}=debug", env!("CARGO_CRATE_NAME")).into()),
+        )
+        .with(tracing_subscriber::fmt::layer())
+        .init();
+
+    let app = Router::new().route("/", get(hello));
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+        .await
+        .unwrap();
+    tracing::debug!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn hello() -> &'static str {
+    "Hello, World!"
+}
